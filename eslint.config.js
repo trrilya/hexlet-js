91b9849e839237d5b1c37f1b21ef43401882a5e7
Titov Ilya <trrkarpov@gmail.com>
2024-09-10 14:36:25 +0500

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,6 @@ const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   ...compat.extends("airbnb"),
 ];
